ad857ab09c3f3c1319b9986615d19e38a07bdb09
moganesan <developer.moganesan>
2023-11-01 22:38:22 +0530

    projects component added

--- components/projects.js
+++ components/projects.js
@@ -1,97 +1,164 @@
-import React, { useLayoutEffect } from "react";
+import HorizontalScroll from "react-scroll-horizontal";
 import Image from "next/image";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import KlvinLogo from "../public/work/klvien/logo.black.svg";
-import NanbanLogo from "../public/work/nanban/logo.svg";
-import KlvienImage from "../public/platforms.jpg";
-import NanbanScreen from "../public/work/nanban/frame.jpg";
-import ReactLogo from "../public/technologyIcons/react.svg";
-import ReduxLogo from "../public/technologyIcons/redux.svg";
-import FirebaseLogo from "../public/technologyIcons/firebase.svg";
-// import "@splidejs/splide/dist/css/splide-core.min.css";
-import "@splidejs/splide/dist/css/splide.min.css";
+import { useEffect, useState } from "react";
+import { ForwardIcon, BackwardIcon } from "@heroicons/react/24/outline";
 
 const Projects = () => {
+  const [projects, setProject] = useState([
+    {
+      id: 1,
+      name: "Aurora Tip Box",
+      category: "Web Application",
+      link: "www.auroratipbot.xyz",
+      image: "https://auroratipbot.xyz/botIcon.svg",
+      description: (
+        <p>
+          Summary: The primary objective of this project is to develop a
+          comprehensive system for tipping Aurora tokens to Telegram users. This
+          multifaceted project consists of three core components: Frontend,
+          Backend (Telegram Bot Server), and Smart Contract. 1. Frontend: The
+          frontend serves as the user interface for this system and plays a
+          crucial role in facilitating token transactions. Users are required to
+          connect their Metamask wallet to the website to get started. Once
+          connected, they can deposit Aurora tokens into the system. The website
+          is seamlessly integrated with the Aurora Tip Bot smart contract,
+          enabling users to initiate deposits. Initially, the balance is tracked
+          using the wallet address. After depositing tokens, users gain the
+          ability to tip specified amounts to Telegram users. 2. Backend
+          (Telegram Bot Server): The backend component is responsible for
+          managing the Aurora Telegram bot. To engage with the system, users
+          must first link their Metamask wallet to their Telegram account. This
+          is achieved by sending a "/login walletaddress request to the Aurora
+          Community Tip Bot deployed within the Telegram environment. The bot
+          responds with a login URL upon successful connection. Once the wallet
+          address is linked to Telegram, users can utilize the "/balance"
+          command to check their token balance. To initiate tips to other
+          Telegram users, they can visit any public group and use the "/tip{" "}
+          amount recipientusername" command. The bot takes care of the
+          transaction, updating the balance within the contract. Subsequently,
+          recipients can withdraw their tipped tokens through the frontend
+          interface. In summary, this project's goal is to create a seamless and
+          user-friendly ecosystem for tipping Aurora tokens to Telegram users.
+          The Frontend, Backend, and Smart Contract components work to enable
+          users to connect their wallets, deposit tokens, and effortlessly send
+          tips to their fellow Telegram users, enhancing the utility and
+          accessibility of Aurora tokens within the Telegram community.
+        </p>
+      ),
+      tags: [
+        "Next.js",
+        "Tailwindcss",
+        "Solidity",
+        "Ethers.js",
+        "Hardhat",
+        "AWS",
+        "Node.js",
+        "Telegram API",
+      ],
+    },
+  ]);
+
+  const [selectedProject, setSelectedProject] = useState(0);
+
+  const forward = () => {
+    setSelectedProject((prev) =>
+      prev == projects.length - 1 ? prev : prev + 1
+    );
+  };
+
+  const backward = () => {
+    setSelectedProject((prev) => (prev === 0 ? prev : prev - 1));
+  };
+
+  useEffect(() => {
+    console.log("Selected Product", projects[selectedProject]);
+  }, []);
   return (
-    <Splide
-      className="bg-purpleLight"
-      options={{
-        arrows: false,
-        autoplay: true,
-        perPage: 1,
-        rewind: true,
-        gap: "1rem",
-      }}
-    >
-      <SplideSlide>
-        <div className="grid smb:grid-cols-1 sm:grid-cols-2 smb:p-5 sm:p-20 p-20 items-center dark:text-black2 self-center">
-          <div>
-            <h2 className="text-black2 font-bold smb:text-2xl sm:text-4xl text-4xl">
-              Recent Work
-            </h2>
-            <div className="w-52 smb:w-40 sm:w-52 smb:mt-3 sm:mt-6">
-              <Image alt="nanban" src={NanbanLogo} />
-            </div>
-            <p className="w-full text-2xl sm:mt-11">
-              A social media platform for schools and college students.
-            </p>
-            <div className="flex smb:mt-5 sm:mt-10 items-center">
-              <div className="w-16">
-                <Image alt="react" width={100} src={ReactLogo} />
-              </div>
-              <div className="w-16 ml-5">
-                <Image alt="redux" width={100} src={ReduxLogo} />
-              </div>
-              <div className="w-16 ml-5">
-                <Image alt="firebase" width={100} src={FirebaseLogo} />
-              </div>
-            </div>
-          </div>
-          <div className="smb:mt-5">
-            <Image alt="nanban-screen" src={NanbanScreen} />
-          </div>
+    <div className="mt-20 px-44">
+      <span className="text-text1 text-4xl font-bold flex items-center justify-center">
+        {"<h1>"}
+        <h1 className="text-4xl text-grey font-bold">Projects</h1>
+        {"</h1>"}
+      </span>
+      <div className="flex items-center justify-between mt-9">
+        <div className="flex-grow w-1/2">
+          <Image src={"/about.jpeg"} width={300} height={300} />
         </div>
-      </SplideSlide>
-      <SplideSlide>
-        <div className="grid smb:grid-cols-1 sm:grid-cols-2 smb:p-5 sm:p-20 p-20 items-center dark:text-black2 self-center">
-          <div>
-            <h2 className="text-black2 font-bold smb:text-2xl sm:text-4xl text-4xl">
-              Recent Work
-            </h2>
-            <div className="w-52 smb:w-40 sm:w-52 smb:mt-3 sm:mt-6">
-              <Image alt="klvien" src={KlvinLogo} />
-            </div>
-            <p className="w-full text-2xl sm:mt-11">
-              Complete School and College Mangement System.
-            </p>
-            <div className="flex smb:mt-5 sm:mt-10 items-center">
-              <div className="w-16">
-                <Image alt="react" width={100} src={ReactLogo} />
-              </div>
-              <div className="w-16 ml-5">
-                <Image alt="redux" width={100} src={ReduxLogo} />
-              </div>
-              <div className="w-16 ml-5">
-                <Image alt="firebase" width={100} src={FirebaseLogo} />
-              </div>
-            </div>
-            <div className="mt-5">
-              <a
-                rel="noreferrer"
-                href="https://klvien.vercel.app/"
-                target="_blank"
-                className="border-2 p-2 rounded-xl border-purple hover:bg-purple hover:text-white duration-500 bg-white font-bold text-sm text-purple cursor-pointer"
+        <div className="flex-grow w-1/2 max-w-1/2 p-4">
+          <h1 className="font-Russo-One text-md">
+            {projects[selectedProject]?.category}
+          </h1>
+          <h2 className="font-Russo-One text-green text-2xl">
+            {projects[selectedProject]?.name}
+          </h2>
+          {projects[selectedProject]?.description}
+
+          <div className="grid grid-flow-row gap-10 mt-10 grid-cols-4">
+            {projects[selectedProject]?.tags.map((tag) => {
+              return (
+                <div>
+                  <span className="px-5 py-2 border bg-greenBackground border-transparent text-center hover:border-green duration-500 overflow-hidden whitespace-nowrap text-ellipsis">
+                    {tag}
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+          <div className="mt-10">
+            <a
+              href={projects[selectedProject].link}
+              target="_blank"
+              className="px-6 py-2 border border-green hover:bg-greenBackground2 duration-500"
+            >
+              View Project
+            </a>
+          </div>
+          <div className="flex items-center mt-10">
+            <div>
+              <svg
+                width="93"
+                height="81"
+                viewBox="0 0 93 81"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                View Project
-              </a>
+                <path
+                  d="M68.75 79H24.25L2 40.5L24.25 2H68.75L91 40.5L68.75 79Z"
+                  stroke="#01FF94"
+                  strokeWidth="3"
+                />
+                <foreignObject x="10" y="10" width="200" height="100">
+                  <div
+                    xmlns="http://www.w3.org/1999/xhtml"
+                    className="h-16 flex items-center justify-center w-16"
+                  >
+                    <div className="flex ml-3 font-Russo-One text-xl">
+                      <span className="text-green">{selectedProject + 1}</span>
+                      {"/"}
+                      <span>{projects.length}</span>
+                    </div>
+                  </div>
+                </foreignObject>
+              </svg>
+            </div>
+            <div className="ml-10">
+              <button>
+                <BackwardIcon
+                  onClick={backward}
+                  className="w-11 h-11 text-green"
+                />
+              </button>
+              <button className="ml-4">
+                <ForwardIcon
+                  onClick={forward}
+                  className="w-11 h-11 text-green"
+                />
+              </button>
             </div>
           </div>
-          <div className="smb:mt-10">
-            <Image alt="klvien" src={KlvienImage} />
-          </div>
         </div>
-      </SplideSlide>
-    </Splide>
+      </div>
+    </div>
   );
 };
 
