d99e4d4bf43eb152397769b013b3196ccfb9ee34
moganesan <developer.moganesan>
2023-11-02 13:49:06 +0530

    deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
+/** @type {import('next').NextConfig} */
+
 module.exports = {
-  presets: ["next/babel"],
-  reactStrictMode: true,
-  images: {
-    loader: "imgix",
-    path: "/",
-  },
-  output: "export",
   images: {
     unoptimized: true,
   },
